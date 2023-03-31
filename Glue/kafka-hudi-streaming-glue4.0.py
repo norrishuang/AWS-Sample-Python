2442053924cffb4f68f91d3ff45ef45a82f1c8c7
Norris <33706975+norrishuang@users.noreply.github.com>
2023-03-31 10:31:29 +0800

    Synchronizing job kafka-hudi-streaming-glue4.0 from AWS Glue at 2023/03/31 02:31:29

---
+++ Glue/kafka-hudi-streaming-glue4.0.py
@@ -0,0 +1,218 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.sql.session import SparkSession
+from awsglue.context import GlueContext
+
+from awsglue.job import Job
+from datetime import datetime
+from awsglue import DynamicFrame
+from pyspark.sql.functions import col, from_json, schema_of_json, current_timestamp
+from pyspark.sql.types import StructType, StructField, StringType, LongType, IntegerType
+
+'''
+Glue Hudi Test
+通过 Glue 消费Kafka的数据，写S3（Hudi）。多表，支持I U D
+'''
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+
+
+HUDI_FORMAT = "org.apache.hudi"
+config = {
+    "table_name": "user_order_list",
+    "database_name": "hudi",
+    "target": "s3://myemr-bucket-01/data/hudi/user_order_list",
+    "primary_key": "id",
+    "sort_key": "id",
+    "commits_to_retain": "4",
+    "streaming_db": "kafka_db",
+    "streaming_table": "kafka_iceberg_norrisdb_01"
+}
+
+additional_options={
+    "hoodie.table.name": config['table_name'],
+    "className" : "org.apache.hudi",
+    "hoodie.datasource.write.storage.type": "COPY_ON_WRITE",
+    "hoodie.datasource.write.operation": "upsert",
+    "hoodie.datasource.write.recordkey.field": config["primary_key"],
+    "hoodie.datasource.write.precombine.field": config["sort_key"],
+    "hoodie.datasource.hive_sync.enable": "true",
+    "hoodie.datasource.hive_sync.database": config['database_name'],
+    "hoodie.datasource.hive_sync.table": config['table_name'],
+    "hoodie.datasource.hive_sync.partition_extractor_class": "org.apache.hudi.hive.MultiPartKeysValueExtractor",
+    "hoodie.datasource.hive_sync.use_jdbc": "false",
+    "hoodie.datasource.hive_sync.mode": "hms",
+    "path": config['target']
+}
+
+#源表对应iceberg目标表（多表处理）
+tableIndexs = {
+    "portfolio": "iceberg_portfolio_10",
+    "portfolio_02": "portfolio_02",
+    "table02": "table02",
+    "table01": "table01",
+    "user_order_list_small_file": "user_order_list_small_file",
+    "user_order_list": "user_order_list",
+    "user_order_main": "user_order_main",
+    "user_order_mor": "user_order_mor",
+    "tb_schema_evolution": "tb_schema_evolution"
+}
+
+
+spark = SparkSession.builder.config('spark.serializer','org.apache.spark.serializer.KryoSerializer').getOrCreate()
+glueContext = GlueContext(spark.sparkContext)
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+logger = glueContext.get_logger()
+logger.info('Initialization.')
+
+# S3 sink locations
+output_path = "s3://myemr-bucket-01/data/"
+job_time_string = datetime.now().strftime("%Y%m%d%")
+s3_target = output_path + job_time_string
+checkpoint_location = args["TempDir"] + "/" + args['JOB_NAME'] + "/checkpoint/" + "checkpoint-02" + "/"
+
+# 把 dataframe 转换成字符串，在logger中输出
+def getShowString(df, n=10, truncate=True, vertical=False):
+    if isinstance(truncate, bool) and truncate:
+        return df._jdf.showString(n, 10, vertical)
+    else:
+        return df._jdf.showString(n, int(truncate), vertical)
+
+def processBatch(data_frame,batchId):
+    if (data_frame.count() > 0):
+        schema = StructType([
+            StructField("before", StringType(), True),
+            StructField("after", StringType(), True),
+            StructField("source", StringType(), True),
+            StructField("op", StringType(), True),
+            StructField("ts_ms", LongType(), True),
+            StructField("transaction", StringType(), True)
+        ])
+
+        dataJsonDF = data_frame.select(from_json(col("$json$data_infer_schema$_temporary$").cast("string"), schema).alias("data")).select(col("data.*"))
+        # logger.info("############  Create DataFrame  ############### \r\n" + getShowString(dataJsonDF,truncate = False))
+
+        dataInsert = dataJsonDF.filter("op in ('c','r') and after is not null")
+        # 过滤 区分 insert upsert delete
+        dataUpsert = dataJsonDF.filter("op in ('u') and after is not null")
+
+        dataDelete = dataJsonDF.filter("op in ('d') and before is not null")
+
+        if(dataInsert.count() > 0):
+            #### 分离一个topics多表的问题。
+            # dataInsert = dataInsertDYF.toDF()
+            sourceJson = dataInsert.select('source').first()
+            schemaSource = schema_of_json(sourceJson[0])
+
+            # 获取多表
+            dataTables = dataInsert.select(from_json(col("source").cast("string"),schemaSource).alias("SOURCE")) \
+                .select(col("SOURCE.db"),col("SOURCE.table")).distinct()
+            # logger.info("############  MutiTables  ############### \r\n" + getShowString(dataTables,truncate = False))
+            rowTables = dataTables.collect()
+
+            for cols in rowTables :
+                tableName = cols[1]
+                dataDF = dataInsert.select(col("after"), \
+                                           from_json(col("source").cast("string"),schemaSource).alias("SOURCE")) \
+                    .filter("SOURCE.table = '" + tableName + "'")
+                dataJson = dataDF.select('after').first()
+                schemaData = schema_of_json(dataJson[0])
+                logger.info("############  Insert Into-GetSchema-FirstRow:" + dataJson[0])
+
+                dataDFOutput = dataDF.select(from_json(col("after").cast("string"),schemaData).alias("DFADD")).select(col("DFADD.*"), current_timestamp().alias("ts"))
+                # logger.info("############  INSERT INTO  ############### \r\n" + getShowString(dataDFOutput,truncate = False))
+                InsertDataLake(tableName, dataDFOutput)
+
+        if(dataUpsert.count() > 0):
+            #### 分离一个topics多表的问题。
+            # dataUpsert = dataUpsertDYF.toDF()
+            sourceJson = dataUpsert.select('source').first()
+            schemaSource = schema_of_json(sourceJson[0])
+
+            # 获取多表
+            dataTables = dataUpsert.select(from_json(col("source").cast("string"),schemaSource).alias("SOURCE")) \
+                .select(col("SOURCE.db"),col("SOURCE.table")).distinct()
+            # logger.info("############  MutiTables  ############### \r\n" + getShowString(dataTables,truncate = False))
+            rowTables = dataTables.collect()
+
+            for cols in rowTables :
+                tableName = cols[1]
+                dataDF = dataUpsert.select(col("after"), \
+                                           from_json(col("source").cast("string"),schemaSource).alias("SOURCE")) \
+                    .filter("SOURCE.table = '" + tableName + "'")
+
+                ##由于merge into schema顺序的问题，这里schema从表中获取（顺序问题待解决）
+                database_name = config["database_name"]
+                table_name = tableIndexs[tableName]
+                schemaData = spark.table(f"glue_catalog.{database_name}.{table_name}").schema
+                # dataJson = dataDF.select('after').first()
+                # schemaData = schema_of_json(dataJson[0])
+
+                dataDFOutput = dataDF.select(from_json(col("after").cast("string"),schemaData).alias("DFADD")).select(col("DFADD.*"), current_timestamp().alias("ts"))
+                logger.info("############  MERGE INTO  ############### \r\n" + getShowString(dataDFOutput,truncate = False))
+                InsertDataLake(tableName, dataDFOutput)
+
+
+        if(dataDelete.count() > 0):
+            # dataDelete = dataDeleteDYF.toDF()
+            sourceJson = dataDelete.select('source').first()
+            # schemaData = schema_of_json([rowjson[0]])
+
+            schemaSource = schema_of_json(sourceJson[0])
+            dataTables = dataDelete.select(from_json(col("source").cast("string"),schemaSource).alias("SOURCE")) \
+                .select(col("SOURCE.db"),col("SOURCE.table")).distinct()
+
+            # logger.info("############  Auto Schema Recognize  ############### \r\n" + getShowString(dataDelete,truncate = False))
+
+            rowTables = dataTables.collect()
+            for cols in rowTables :
+                tableName = cols[1]
+                dataDF = dataDelete.select(col("before"), \
+                                           from_json(col("source").cast("string"),schemaSource).alias("SOURCE")) \
+                    .filter("SOURCE.table = '" + tableName + "'")
+                dataJson = dataDF.select('before').first()
+
+                schemaData = schema_of_json(dataJson[0])
+                dataDFOutput = dataDF.select(from_json(col("before").cast("string"),schemaData).alias("DFDEL")).select(col("DFDEL.*"))
+                # logger.info("############  DELETE FROM  ############### \r\n" + getShowString(dataDFOutput,truncate = False))
+                InsertDataLake(tableName,dataDFOutput)
+
+def InsertDataLake(tableName,dataFrame):
+    # logger.info("##############  Func:InputDataLake [ "+ tableName +  "] ############# \r\n"
+    #             + getShowString(dataFrame,truncate = False))
+
+    database_name = config["database_name"]
+    table_name = tableIndexs[tableName]
+
+    # dyDataFrame = DynamicFrame.fromDF(dataFrame, glueContext, "from_data_frame").toDF();
+
+    # dyDataFrame = DynamicFrame.fromDF(dataFrame, glueContext, "from_data_frame").toDF();
+    # dyDataFrame.writeTo(f"glue_catalog.{database_name}.{table_name}") \
+    #     .option("merge-schema", "true") \
+    #     .option("check-ordering","false").append()
+
+    glueContext.write_dynamic_frame.from_options(frame = DynamicFrame.fromDF(dataFrame, glueContext, "outputDF"),
+                                                 connection_type = "custom.spark",
+                                                 connection_options = additional_options)
+
+# Script generated for node Apache Kafka
+dataframe_ApacheKafka_node1670731139435 = glueContext.create_data_frame.from_catalog(
+    database = config["streaming_db"],
+    table_name = config["streaming_table"],
+    additional_options = {"startingOffsets": "earliest", "inferSchema": "true"},
+    transformation_ctx = "dataframe_ApacheKafka_node1670731139435",
+)
+
+glueContext.forEachBatch(frame = dataframe_ApacheKafka_node1670731139435,
+                         batch_function = processBatch,
+                         options = {
+                             "windowSize": "30 seconds",
+                             "checkpointLocation": checkpoint_location,
+                             "batchMaxRetries": 1
+                         })
+
+job.commit()
